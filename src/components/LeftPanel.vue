434ea3c44e50a9ab269c4f45e18eb514e47b67c7
yuanquan <yuanmouquan@zving.com>
2019-04-03 19:27:26 +0800

    删除左面板无用的list ，一系列li标签，使用循环循环出来的li来替换

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -19,73 +19,16 @@
           </select>
           <p>'option的值':{{option}}</p>
           <!-- list-->
-        <!--  <ul id="divSnippetList" @click="moveLeftForm($event)">
-            <li v-show="(option === '输入框'|| option==='all')" data-id="0" data-snipcat=",all," class="ui-draggable">
-              <img alt="" src="../assets/images/fl1.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="1" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft1.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="2" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft8.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="3" data-snipcat=",输入框," class="ui-draggable">
-              <img src="../assets/images/ft9.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="4" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft10.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')"  data-id="5" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft3.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="6" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft4.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="7" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft7.png">
-            </li>
-            <li v-show="(option === '输入框'|| option==='all')" data-id="8" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs1.png">
-            </li>
-
-            <li v-show="(option === '列表选择'|| option==='all')" data-id="9" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs3.png">
-            </li>
-            <li v-show="(option === '列表选择'|| option==='all')" data-id="10" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs2.png">
-            </li>
-
-            <li v-show="(option  === '单选、多选'|| option==='all' )" data-id="11" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc1.png">
-            </li>
-            <li v-show="(option  === '单选、多选'|| option==='all' )" data-id="12" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc2.png">
-            </li>
-            <li v-show="(option  === '单选、多选'|| option==='all')" data-id="13" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc3.png">
-            </li>
-            <li v-show="(option  === '单选、多选'|| option==='all' )" data-id="14" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc4.png">
-            </li>
-            <li v-show="(option  === '单选、多选'|| option==='all')" data-id="15" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc5.png">
-            </li>
-
-            <li v-show="(option === '文件、按钮'|| option==='all')" data-id="16" data-snipcat=",文件、按钮," class="ui-draggable">
-              <img alt="" src="../assets/images/ff1.png">
-            </li>
-            <li v-show="(option === '文件、按钮'|| option==='all')" data-id="17" data-snipcat=",文件、按钮," class="ui-draggable">
-              <img alt="" src="../assets/images/fb1.png">
-            </li>
-          </ul>-->
-
           <ul id="divSnippetList">
-            <draggable v-model="leftForms" >
-                <li v-for="item in leftForms"
-                    :key="item.id"
-                    v-html="item.leftCont"
-                    @click="moveLeftForm">
-                  {{item.id}}
+            <draggable v-model="leftForms" :options="comOption">
+                <li v-for="item in secLeftForms"
+                    class="ui-draggable"
+                    :key="item.data_id"
+                    :data_id="item.data_id"
+                    @click="moveLeftForm"
+                    :aaa="item.type"
+                    v-show="(option===item.type || option==='all')">
+                  <img :src="item.src" alt="" >
                 </li>
             </draggable>
           </ul>
@@ -108,135 +51,143 @@
         leftIndex:'',
         leftForms:[
           {id:0,leftCont:` <li v-show="(option === '输入框'|| option==='all')" data-snipid="0" data-snipcat=",all," class="ui-draggable">
-              <img alt="" src="../assets/images/fl1.png">
+              <img alt="" src="../../static/images/fl1.png">
             </li>`},
           {id:1,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="1" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft1.png">
+              <img alt="" src="../../static/images/ft1.png">
             </li>`},
           {id:2,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="2" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft8.png">
+              <img alt="" src="../../static/images/ft8.png">
             </li>`},
           {id: 3,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="3" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft9.png">
+              <img alt="" src="../../static/images/ft9.png">
             </li>`},
           {id:4,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="4" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft10.png">
+              <img alt="" src="../../static/images/ft10.png">
             </li>`},
           {id:5,leftCont:`<li v-show="(option === '输入框'|| option==='all')"  data-snipid="5" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft3.png">
+              <img alt="" src="../../static/images/ft3.png">
             </li>`},
           {id:6,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="6" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft4.png">
+              <img alt="" src="../../static/images/ft4.png">
             </li>`},
           {id:7,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="7" data-snipcat=",输入框," class="ui-draggable">
-              <img alt="" src="../assets/images/ft7.png">
+              <img alt="" src="../../static/images/ft7.png">
             </li>`},
           {id:8,leftCont:`<li v-show="(option === '输入框'|| option==='all')" data-snipid="8" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs1.png">
+              <img alt="" src="../../static/images/fs1.png">
             </li>`},
           {id:9,leftCont:`<li v-show="(option === '列表选择'|| option==='all')" data-snipid="9" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs3.png">
+              <img alt="" src="../../static/images/fs3.png">
             </li>`},
           {id:10,leftCont:`<li v-show="(option === '列表选择'|| option==='all')" data-snipid="10" data-snipcat=",列表选择," class="ui-draggable">
-              <img alt="" src="../assets/images/fs2.png">
+              <img alt="" src="../../static/images/fs2.png">
             </li>`},
           {id:11,leftCont:`<li v-show="(option  === '单选、多选'|| option==='all' )" data-snipid="11" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc1.png">
+              <img alt="" src="../../static/images/fc1.png">
             </li>`},
           {id:12,leftCont:`<li v-show="(option  === '单选、多选'|| option==='all' )" data-snipid="12" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc2.png">
+              <img alt="" src="../../static/images/fc2.png">
             </li>`},
           {id:13,leftCont:`<li v-show="(option  === '单选、多选'|| option==='all')" data-snipid="13" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc3.png">
+              <img alt="" src="../../static/images/fc3.png">
             </li>`},
           {id:14,leftCont:`<li v-show="(option  === '单选、多选'|| option==='all' )" data-snipid="14" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc4.png">
+              <img alt="" src="../../static/images/fc4.png">
             </li>`},
           {id:15,leftCont:`<li v-show="(option  === '单选、多选'|| option==='all')" data-snipid="15" data-snipcat=",单选、多选," class="ui-draggable">
-              <img alt="" src="../assets/images/fc5.png">
+              <img alt="" src="../../static/images/fc5.png">
             </li>`},
           {id:16,leftCont:`<li v-show="(option === '文件、按钮'|| option==='all')" data-snipid="16" data-snipcat=",文件、按钮," class="ui-draggable">
-              <img alt="" src="../assets/images/ff1.png">
+              <img alt="" src="../../static/images/ff1.png">
             </li>`},
           {id:17,leftCont:`<li v-show="(option === '文件、按钮'|| option==='all')" data-snipid="17" data-snipcat=",文件、按钮," class="ui-draggable">
-              <img alt="" src="../assets/images/fb1.png">
+              <img alt="" src="../../static/images/fb1.png">
             </li>`}
         ],
+        // 左面板数据
         secLeftForms:[
-          {'data-id':0,
-            src:'../assets/images/fl1.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':0,
+            'src':'../../static/images/fl1.png',
+            'type':'输入框'
           },
-          {'data-id':1,
-            src:'../assets/images/ft1.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':1,
+            'src':'../../static/images/ft1.png',
+            'type':'输入框'
           },
-          {'data-id':2,
-            src:'../assets/images/ft8.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':2,
+            'src':'../../static/images/ft8.png',
+            'type':'输入框'
           },
-          {'data-id':3,
-            src:'../assets/images/ft9.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':3,
+            'src':'../../static/images/ft9.png',
+            'type':'输入框'
           },
-          {'data-id':4,
-            src:'../assets/images/ft10.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':4,
+            'src':'../../static/images/ft10.png',
+            'type':'输入框'
           },
-          {'data-id':5,
-            src:'../assets/images/ft3.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':5,
+            'src':'../static/images/ft3.png',
+            'type':'输入框'
           },
-          {'data-id':6,
-            src:'../assets/images/ft4.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':6,
+            'src':'../static/images/ft4.png',
+            'type':'输入框'
           },
-          {'data-id':7,
-            src:'../assets/images/ft7.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':7,
+            'src':'../static/images/ft7.png',
+            'type':'输入框'
           },
-          {'data-id':8,
-            src:'../assets/images/fs1.png',
-            show:'(option === \'输入框\'|| option===\'all\')',
+          {'data_id':8,
+            'src':'../static/images/fs1.png',
+            'type':'输入框'
           },
-          {'data-id':9,
-            src:'../assets/images/fs3.png',
-            show:'(option === \'列表选择\'|| option===\'all\')',
+          {'data_id':9,
+            'src':'../static/images/fs3.png',
+            'type':'列表选择'
           },
-          {'data-id':10,
-            src:'../assets/images/fs2.png',
-            show:'(option === \'列表选择\'|| option===\'all\')',
+          {'data_id':10,
+            'src':'../static/images/fs2.png',
+            'type':'列表选择'
           },
-          {'data-id':11,
-            src:'../assets/images/fc1.png',
-            show:'(option  === \'单选、多选\'|| option===\'all\' )',
+          {'data_id':11,
+            'src':'../static/images/fc1.png',
+            'type':'列表选择'
           },
-          {'data-id':12,
-            src:'../assets/images/fc2.png',
-            show:'(option  === \'单选、多选\'|| option===\'all\' )',
+          {'data_id':12,
+            'src':'../static/images/fc2.png',
+            'type':'列表选择'
           },
-          {'data-id':13,
-            src:'../assets/images/fc3.png',
-            show:'(option  === \'单选、多选\'|| option===\'all\' )',
+          {'data_id':13,
+            'src':'../static/images/fc3.png',
+            'type':'单选、多选'
           },
-          {'data-id':14,
-            src:'../assets/images/fc4.png',
-            show:'(option  === \'单选、多选\'|| option===\'all\' )',
+          {'data_id':14,
+            'src':'../static/images/fc4.png',
+            'type':'单选、多选'
           },
-          {'data-id':15,
-            src:'../assets/images/fc5.png',
-            show:'(option  === \'单选、多选\'|| option===\'all\' )',
+          {'data_id':15,
+            'src':'../static/images/fc5.png',
+            'type':'单选、多选'
           },
-          {'data-id':16,
-            src:'../assets/images/ff1.png',
-            show:'(option === \'文件、按钮\'|| option===\'all\')',
+          {'data_id':16,
+            'src':'../static/images/ff1.png',
+            'type':'文件、按钮'
           },
-          {'data-id':17,
-            src:'../assets/images/fb1.png',
-            show:'(option === \'文件、按钮\'|| option===\'all\')',
+          {'data_id':17,
+            'src':'../static/images/fb1.png',
+            'type':'文件、按钮'
           },
 
-        ]
+        ],
+        // vue.draggble配置信息
+        comOption:{
+          sort:true,
+          clone:true,
+          group:'leftArray',
+          animation:300,
+        }
       }
     },
     mounted(){},
